73dd06fa82c25346c298abb68b40ac38c1875f3d
Ahmed El-Gaafrawy <ahmed.elgaafrawy111@gmail.com>
2020-04-09 18:39:43 +0200

    AVR_atmega_32 - Portable

---
+++ APP/main.c
@@ -0,0 +1,52 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 7, 2020
+ *      Author: hp
+ */
+#include "../LIBRARY/stdTypes.h"
+#include "../LIBRARY/BIT_MATH.h"
+#include "../LIBRARY/errorStates.h"
+#include "../LIBRARY/AVR_REG.h"
+
+#include "../MCAL/GIE_int.h"
+#include "../MCAL/ADC_int.h"
+#include <util/delay.h>
+
+void adc (void*);
+volatile u8 adcRead;
+int main ()
+{
+	u8 a= 15;
+	void*ptr=&a;
+	ADC_enuInit();
+	DDRB=0xff;
+	DDRA=0x00;
+	GIE_enuEnable();
+	ADC_enuEnable();
+	ADC_enuSelectChannel(0);
+	ADC_enuEnableInterrupt();
+	ADC_enuDisableTrigger();
+	ADC_enuCallBackFunction(adc ,ptr);
+	while (1)
+	{
+		ADC_enuStartConversion();
+		//ADC_enuStartConversion();
+		//ADC_enuPollingOnFlag();
+
+	}
+
+	return 0;
+}
+
+void adc (void*f)
+{
+	u8 * pty;
+	pty=(u8*)f;
+	ADC_enuReadHighRegister(&adcRead);
+	PORTB=adcRead;
+	//PORTB=*pty;
+	//ADC_enuStartConversion();
+	//PORTB|=(1<<7);
+}
+
